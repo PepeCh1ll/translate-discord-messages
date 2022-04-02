9b87b29486e527bff61a58339da86a458e3dfa90
PepeCh1ll <kotohleubshek@mail.ru>
2022-04-02 23:22:01 +0500

    Добавлен UI

--- main.py
+++ main.py
@@ -1,67 +1,104 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from window import Ui_MainWindow
+import sys
 import requests
 from googletrans import Translator
 import json
 import time
 import logging
 import traceback
+from threading import Thread
 
-logger = logging.getLogger('Logs')
-logger.setLevel(logging.INFO)
+class mywindow(QtWidgets.QMainWindow):
+	def translate(self):
+		logger = logging.getLogger('Logs')
+		logger.setLevel(logging.INFO)
 
-log = logging.FileHandler('logs.txt', encoding='utf-8')
-log.setFormatter(logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s'))
+		log = logging.FileHandler('logs.txt', encoding='utf-8')
+		log.setFormatter(logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s'))
 
-logger.addHandler(log)
+		logger.addHandler(log)
 
+		self.ui.lineEdit.setReadOnly(True)
+		self.ui.lineEdit_2.setReadOnly(True)
+		self.ui.lineEdit_3.setReadOnly(True)
+		self.ui.lineEdit_4.setReadOnly(True)
 
-chat_id = input('Введите ID канала на сервере: ')
-author_id = input('Введите ID вашего аккаунта: ')
-lang =input('Введите язык чата в формате \'ru\': ')
+		chat_id = self.ui.lineEdit.text()
+		author_id = self.ui.lineEdit_2.text()
+		lang = self.ui.lineEdit_3.text()
 
-header = {'authorization': input('Введите токен вашего аккаунта: ')}
+		header = {'authorization': self.ui.lineEdit_4.text()}
 
-message_id = ''
+		message_id = ''
 
-t = Translator()
+		t = Translator()
 
-print('\nДля выхода из программы просто закройте её')
-
-try:
-	with requests.Session() as s:
-		r = s.get(f'https://discord.com/api/v9/channels/{chat_id}/messages?limit=3', headers=header)
-		result = json.loads(r.text)
-		for i in result:
-			if i['author']['id'] == author_id:
-				message = i['content']
-				msg_id = i['id']
-				break
-		message_id = msg_id
-
-		logger.info('Старт программы')
-		while True:
-			r = s.get(f'https://discord.com/api/v9/channels/{chat_id}/messages?limit=3', headers=header)
-			result = json.loads(r.text)
-			for i in result:
-				if i['author']['id'] == author_id:
-					message = i['content']
-					msg_id = i['id']
-					break
-			if msg_id == message_id:
-				continue
-			else:
+		try:
+			with requests.Session() as s:
+				r = s.get(f'https://discord.com/api/v9/channels/{chat_id}/messages?limit=3', headers=header)
+				result = json.loads(r.text)
+				for i in result:
+					if i['author']['id'] == author_id:
+						message = i['content']
+						msg_id = i['id']
+						break
 				message_id = msg_id
 
-			logger.info('Принято сообщение: ' + message)
-			message = t.translate(message, dest=lang).text
-			payload = {'content': message.capitalize()}
-			r = s.patch(f'https://discord.com/api/v9/channels/{chat_id}/messages/{message_id}', headers=header, json=payload)
-			logger.info('Сообщение переведено: ' + message)
+				logger.info('Старт потока')
+				while True:
+					r = s.get(f'https://discord.com/api/v9/channels/{chat_id}/messages?limit=3', headers=header)
+					result = json.loads(r.text)
+					for i in result:
+						if i['author']['id'] == author_id:
+							message = i['content']
+							msg_id = i['id']
+							break
+					if msg_id == message_id:
+						continue
+					else:
+						message_id = msg_id
 
-			time.sleep(0.3)
+					logger.info('Принято сообщение: ' + message)
+					message = t.translate(message, dest=lang).text
+					payload = {'content': message.capitalize()}
+					r = s.patch(f'https://discord.com/api/v9/channels/{chat_id}/messages/{message_id}', headers=header, json=payload)
+					logger.info('Сообщение переведено: ' + message)
 
-except Exception as e:
-	print(traceback.format_exc())
-	input('Произошла ошибка, просмотрите файл logs.txt или обратитесь за помощью к сотруднику\nНажмите Enter для выхода')
-	logger.error('Произошла ошибка: ' + traceback.format_exc())
-finally:
-	logger.info('Завершение программы')
+					time.sleep(0.3)
+
+		except Exception as e:
+			logger.error('Произошла ошибка: ' + traceback.format_exc())
+
+			self.msg = QMessageBox()
+			self.msg.setWindowTitle("Ошибка")
+			self.msg.setText("Произошла ошибка, просмотрите файл logs.txt или обратитесь за помощью к сотруднику")
+			self.msg.setIcon(QMessageBox.Critical)
+			self.msg.exec_()
+		finally:
+			logger.info('Завершение потока')
+			sys.exit()
+
+	def __init__(self):
+		super(mywindow, self).__init__()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+
+		self.ui.pushButton.clicked.connect(self.starting)
+		self.th = Thread(target=self.translate, daemon=True)
+
+	def starting(self):
+		self.th.start()
+
+		self.msg = QMessageBox()
+		self.msg.setWindowTitle("Внимание")
+		self.msg.setText("Перевод запущен\nДля остановки просто закройте приложение\nНе нажимайте повторну кнопку Старт, это приведет к ошибке")
+		self.msg.setIcon(QMessageBox.Information)
+		self.msg.exec_()
+
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.show()
+
+sys.exit(app.exec())
