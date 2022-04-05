84350f6847158060adba6647c6a72b564ad5eb49
PepeCh1ll <kotohleubshek@mail.ru>
2022-04-05 14:50:50 +0500

    Добавление возможности мультиаккаунтов

--- main.py
+++ main.py
@@ -9,67 +9,67 @@ import time
 import logging
 import traceback
 from threading import Thread
+import configparser
 
 class mywindow(QtWidgets.QMainWindow):
 	def translate(self):
-		logger = logging.getLogger('Logs')
-		logger.setLevel(logging.INFO)
+		self.logger = logging.getLogger('Logs')
+		self.logger.setLevel(logging.INFO)
 
-		log = logging.FileHandler('logs.txt', encoding='utf-8')
-		log.setFormatter(logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s'))
+		self.log = logging.FileHandler('logs.txt', encoding='utf-8')
+		self.log.setFormatter(logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s'))
 
-		logger.addHandler(log)
+		self.logger.addHandler(self.log)
 
 		self.ui.lineEdit.setReadOnly(True)
 		self.ui.lineEdit_2.setReadOnly(True)
 		self.ui.lineEdit_3.setReadOnly(True)
 		self.ui.lineEdit_4.setReadOnly(True)
 
-		chat_id = self.ui.lineEdit.text()
-		author_id = self.ui.lineEdit_2.text()
-		lang = self.ui.lineEdit_3.text()
+		self.chat_id = self.ui.lineEdit.text()
+		self.author_id = self.ui.lineEdit_2.text()
+		self.lang = self.ui.lineEdit_3.text()
 
-		header = {'authorization': self.ui.lineEdit_4.text()}
+		self.header = {'authorization': self.ui.lineEdit_4.text()}
 
-		message_id = ''
+		self.message_id = ''
+		self.msg_id = ''
 
-		t = Translator()
+		self.t = Translator()
 
 		try:
-			with requests.Session() as s:
-				r = s.get(f'https://discord.com/api/v9/channels/{chat_id}/messages?limit=3', headers=header)
-				result = json.loads(r.text)
-				for i in result:
-					if i['author']['id'] == author_id:
-						message = i['content']
-						msg_id = i['id']
+			with requests.Session() as self.s:
+				self.r = self.s.get(f'https://discord.com/api/v9/channels/{self.chat_id}/messages?limit=3', headers=self.header)
+				self.result = json.loads(self.r.text)
+				for i in self.result:
+					if i['author']['id'] == self.author_id:
+						self.message = i['content']
+						self.msg_id = i['id']
 						break
-				message_id = msg_id
+				self.message_id = self.msg_id
 
-				logger.info('Старт потока')
 				while True:
-					r = s.get(f'https://discord.com/api/v9/channels/{chat_id}/messages?limit=3', headers=header)
-					result = json.loads(r.text)
-					for i in result:
-						if i['author']['id'] == author_id:
-							message = i['content']
-							msg_id = i['id']
+					self.r = self.s.get(f'https://discord.com/api/v9/channels/{self.chat_id}/messages?limit=3', headers=self.header)
+					self.result = json.loads(self.r.text)
+					for i in self.result:
+						if i['author']['id'] == self.author_id:
+							self.message = i['content']
+							self.msg_id = i['id']
 							break
-					if msg_id == message_id:
+					if self.msg_id == self.message_id:
 						continue
 					else:
-						message_id = msg_id
+						self.message_id = self.msg_id
 
-					logger.info('Принято сообщение: ' + message)
-					message = t.translate(message, dest=lang).text
-					payload = {'content': message.capitalize()}
-					r = s.patch(f'https://discord.com/api/v9/channels/{chat_id}/messages/{message_id}', headers=header, json=payload)
-					logger.info('Сообщение переведено: ' + message)
-
-					time.sleep(0.3)
+					self.logger.info('Принято сообщение: ' + self.message)
+					self.message = self.t.translate(self.message, dest=self.lang).text
+					self.payload = {'content': self.message.capitalize()}
+					self.r = self.s.patch(f'https://discord.com/api/v9/channels/{self.chat_id}/messages/{self.message_id}', headers=self.header, json=self.payload)
+					self.logger.info('Сообщение переведено: ' + self.message)
+					self.s.cookies.clear()
 
 		except Exception as e:
-			logger.error('Произошла ошибка: ' + traceback.format_exc())
+			self.logger.error('Произошла ошибка: ' + traceback.format_exc())
 
 			self.msg = QMessageBox()
 			self.msg.setWindowTitle("Ошибка")
@@ -77,7 +77,7 @@ class mywindow(QtWidgets.QMainWindow):
 			self.msg.setIcon(QMessageBox.Critical)
 			self.msg.exec_()
 		finally:
-			logger.info('Завершение потока')
+			self.logger.info('Завершение потока')
 			sys.exit()
 
 	def __init__(self):
@@ -85,8 +85,43 @@ class mywindow(QtWidgets.QMainWindow):
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
 
+		self.p = [250, 210, 100, 30]
+		self.w = [500, 250]
+		self.btns = []
+
 		self.ui.pushButton.clicked.connect(self.starting)
 		self.th = Thread(target=self.translate, daemon=True)
+
+		self.parser = configparser.ConfigParser()
+		self.parser.read('users.ini')
+		self.x = 100
+		self.y = 170
+		for el in self.parser.sections():
+			if self.x == 500:
+				self.x = 100
+				self.y += 30
+				self.p[1] += 30
+				self.ui.pushButton.setGeometry(self.p[0], self.p[1], self.p[2], self.p[3]) 
+				self.w[1] += 30
+				self.setFixedSize(self.w[0], self.w[1])
+			self.btn = QtWidgets.QPushButton(self)
+			self.btn.setObjectName(el)
+			self.btns.append(el)
+			self.btn.setText(el)
+			self.btn.setGeometry(self.x, self.y, 100, 30)
+			self.btn.clicked.connect(self.account)
+			self.x += 100
+		self.ui.lineEdit.setText(self.parser['user1']['chat'])
+		self.ui.lineEdit_2.setText(self.parser['user1']['auth'])
+		self.ui.lineEdit_3.setText(self.parser['user1']['language'])
+		self.ui.lineEdit_4.setText(self.parser['user1']['token'])
+
+	def account(self, btns):
+		self.button = QtWidgets.QApplication.instance().sender()
+		self.ui.lineEdit.setText(self.parser[self.button.text()]['chat'])
+		self.ui.lineEdit_2.setText(self.parser[self.button.text()]['auth'])
+		self.ui.lineEdit_3.setText(self.parser[self.button.text()]['language'])
+		self.ui.lineEdit_4.setText(self.parser[self.button.text()]['token'])
 
 	def starting(self):
 		self.th.start()
